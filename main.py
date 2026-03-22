af63bd16d3a81fc93075addd43a395e6e1a85e14
EduardoGuerraL <eaguerra0@gmail.com>
2026-03-22 09:58:47 -0300

    style(splash): improve initial screen design

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-# main.py
-from src.gui.app import NetworkApp
+from src.gui.app import NetworkApp, show_initial_splash
 from src.gui.widgets import get_image_path
 
 if __name__ == "__main__":
+    # 1. Primero la portada (ventana sin bordes)
+    show_initial_splash()
+    
+    # 2. Luego pedimos la imagen
     img_path = get_image_path()
+    
+    # 3. Finalmente abrimos la herramienta de trabajo
     if img_path:
         app = NetworkApp(img_path)
         app.run()
